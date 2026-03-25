0e1c8cd4ffa31ddc3d8cf5a69589e9f735d0bd82
Belisoful <belisoful@icloud.com>
2026-03-25 22:54:54 +0000

    phpunit files

---
+++ tests/test_tools/phpunit_bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * A few common settings for all unit tests.
+ *
+ * Also remember do define the @package attribute for your test class to make it appear under
+ * the right package in unit test and code coverage reports.
+ */
+
+echo("\n     *****   HERE  ******\n\n");
+ require_once(__DIR__ . '/../../vendor/autoload.php');
+ require_once(__DIR__ . '/../../framework/Prado.php');
+
+// Project Includes
+//   None.
